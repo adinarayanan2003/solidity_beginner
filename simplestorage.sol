45b9391b591638a34f13d934a7089b5cad07cc18
adinarayanan2003 <75565462+adinarayanan2003@users.noreply.github.com>
2022-06-12 16:37:14 +0530

    Update simplestorage.sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -1,1 +1,35 @@
+pragma solidity ^0.8.0;
 
+
+contract simplestorage{
+  
+  uint256 favnum;
+  
+  struct People{
+    uint256 favnum;
+    string name;
+  
+  }
+  
+  People[] public parray;
+  
+  mapping(string => uint256) public nameToFavoriteNumber;
+  
+  function store(uint256 _favnum) public{
+      
+      favnum=_favnum;
+    
+  }
+  
+  function retrieve() public view returns(uint256){
+    return favnum;
+  }
+  
+  function addperson(string memory _name,uint256 _favnum) public {
+    parray.push(People(_favnum,_name));
+    nameToFavoriteNumber[_name]=_favnum;
+  
+  }
+
+
+}
