2f7663342a45bb9334f69a0131a0ff48d26e7326
adinarayanan2003 <75565462+adinarayanan2003@users.noreply.github.com>
2022-07-08 23:33:56 +0530

    Update hardhat.config.js

--- simplestorage-in hardhat/hardhat.config.js
+++ simplestorage-in hardhat/hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 require("hardhat-gas-reporter")
+require("solidity-coverage")
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 /**
