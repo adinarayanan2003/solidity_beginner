f425beaef6d1e815639a871ca7ca95e58de9b685
adinarayanan2003 <75565462+adinarayanan2003@users.noreply.github.com>
2022-07-08 22:48:24 +0530

    Add files via upload

---
+++ simplestorage-in hardhat/hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("hardhat-gas-reporter")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+
+const RINKEBY_RPC_URL =
+  process.env.rpc_url  ||
+  "url here"
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "priv key here"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "api here"
+const COINMARKETCAP_API_KEY = process.env.coinmapi
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+}
---
+++ simplestorage-in hardhat/scripts/deploy.js
@@ -0,0 +1,43 @@
+const { ethers, run, network } = require("hardhat") 
+require("dotenv").config();
+
+
+async function main() {
+    const contractfact = await ethers.getContractFactory("simplestorage")
+    console.log("Deploying contract...")
+    const simpleStorage = await contractfact.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    console.log("Check for network");
+    console.log(network.config.chainId);
+    console.log(process.env.etherscanapi);
+    if (network.config.chainId === 4 && process.env.etherscanapi) {
+      console.log("Waiting for the transactions to be mined...");
+      await simpleStorage.deployTransaction.wait(6);
+      console.log("Passing arguments to verify function...");
+      await verify(simpleStorage.address, []);
+    }
+}
+
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
